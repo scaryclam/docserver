81fe39e7d34c32704922119d5663c778cdd537d4
Becky Lewis <becky.lewis@tangentlabs.co.uk>
2014-09-23 20:34:35 +0100

    Added request context processor

--- conf/dev.py
+++ conf/dev.py
@@ -77,3 +77,14 @@ STATIC_URL = '/static/'
 
 GIT_CLONE_DIR = os.path.join(BASE_DIR, 'gitclone')
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+)
+
